58ba8f3386702d664b76f586570b317ddd6ece44
Dragonkeun <Dragonkeun7@gmail.com>
2022-12-05 22:04:52 +0900

    programmers 221205

---
+++ programmers/221205.js
@@ -0,0 +1,10 @@
+// 숫자 찾기
+function solution(num, k) {
+    var num = String(num).split('');
+    for(let i=0; i<num.length; i++) {
+        if (String(k) === num[i]) {
+            return i+1;
+        }
+    }
+    return -1;
+}
