daf32e38b72ce8dfaf68319d8b53d079cf61fcc7
Dragonkeun <Dragonkeun7@gmail.com>
2022-12-19 21:51:17 +0900

    폴더 구조 변경

--- programmers/221209.js
+++
@@ -1,9 +0,0 @@
-// 인덱스 바꾸기
-function solution(my_string, num1, num2) {
-    my_string = my_string.split("");
-    var temp = my_string[num1];
-    my_string[num1] = my_string[num2];
-    my_string[num2] = temp;
-    answer = my_string.join("");
-    return answer;
-}
--- programmers/221211.js
+++
@@ -1,5 +0,0 @@
-// 문자열 정렬하기 (2)
-function solution(my_string) {
-    var answer = my_string.toLowerCase().split('').sort().join('');
-    return answer;
-}
